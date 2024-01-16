659c35b7c34fa70bf03b14681ddcdec671ad600a
rudqls007 <rudqls007@gmail.com>
2024-01-16 20:52:25 +0900

    스프링 웹 개발 기초 - 정적 컨텐츠 - MVC와 템플릿 엔진 - API

--- src/main/java/com/example/hellospring/controller/HelloController.java
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,39 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
 
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    // @ResponseBody 를 사용하면 뷰 리졸버( viewResolver )를 사용하지 않음.
+    // 대신에 HTTP의 BODY에 문자 내용을 직접 반환(HTML BODY TAG를 말하는 것이 아님)
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloAPi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
