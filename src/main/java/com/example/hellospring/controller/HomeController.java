7abd8eac326b73309e68802e5449d4268bdc5df2
rudqls007 <rudqls007@gmail.com>
2024-01-17 23:49:40 +0900

    Home Controller

--- src/main/java/com/example/hellospring/controller/HomeController.java
+++ src/main/java/com/example/hellospring/controller/HomeController.java
@@ -1,4 +1,4 @@
-package hello.hellospring.controller;
+package com.example.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
